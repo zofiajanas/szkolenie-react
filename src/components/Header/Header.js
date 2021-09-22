45478dbaef32d84a3fff6f8c36d117c62e8276f6
Zofia Janas <zofia.janas@eengine.pl>
2021-09-22 10:26:36 +0200

    zamiana importy na

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,25 +1,25 @@
 import React from 'react';
 
-import { HeaderStyled, Link, Image, Paragraph } from './Header.css';
+import * as S from './Header.css';
 
 const Header = props => {
   // const Header = ({ logo }) => {
   return (
-    <HeaderStyled className='App-header'>
-      <Image src={props.logo} className='App-logo' alt='logo' />
+    <S.HeaderStyled className='App-header'>
+      <S.Image src={props.logo} className='App-logo' alt='logo' />
       {/* <img src={logo} className='App-logo' alt='logo' /> */}
-      <Paragraph>
+      <S.Paragraph>
         Edit <code>src/App.js</code> and save to reload.
-      </Paragraph>
-      <Link
+      </S.Paragraph>
+      <S.Link
         className='App-link'
         href='https://reactjs.org'
         target='_blank'
         rel='noopener noreferrer'
       >
         Learn React
-      </Link>
-    </HeaderStyled>
+      </S.Link>
+    </S.HeaderStyled>
   );
 };
 
