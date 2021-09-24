9eaf048e0b3c66447879057bb648f6f87f30b1b0
Zofia Janas <zofia.janas@eengine.pl>
2021-09-24 12:35:02 +0200

    dodanie loadash + debounce w searchu

--- src/components/CharactersList/CharactersList.js
+++ src/components/CharactersList/CharactersList.js
@@ -1,20 +1,28 @@
 import React, { useEffect } from 'react';
 import { useState } from 'react';
 import styled from 'styled-components';
+import _ from 'lodash';
+
 import { API_HP_CHARACTERS } from '../../api';
+import SearchCharacter from './SearchCharacter';
+
+const Wrapper = styled.div`
+  background-color: #250101;
+  padding-top: 40px;
+`;
 
 const List = styled.ul`
   width: 90vw;
   margin: 0 auto;
   display: flex;
   flex-wrap: wrap;
-  padding: 0;
+  padding: 5%;
   list-style: none;
 `;
 
 const SingleItem = styled.li`
-  width: 25vw;
-  background-color: #ccc;
+  width: 30%;
+  background-color: #e9e7e7;
   margin: 20px;
   padding: 40px;
 
@@ -25,6 +33,7 @@ const SingleItem = styled.li`
 `;
 
 const CharactersList = () => {
+  const [basicCharacters, setBasicCharacter] = useState([]);
   const [characters, setCharacter] = useState([]);
 
   const getCharacters = async () => {
@@ -32,6 +41,7 @@ const CharactersList = () => {
       const resp = await fetch(API_HP_CHARACTERS);
       const data = await resp.json();
       setCharacter(data);
+      setBasicCharacter(data);
     } catch (e) {
       console.log(e);
     }
@@ -43,6 +53,13 @@ const CharactersList = () => {
     //   })
     //   .catch(err => console.log(err));
   };
+
+  const searchCharacter = _.debounce(value => {
+    const searchResult = basicCharacters.filter(char =>
+      char.name.toUpperCase().includes(value.toUpperCase())
+    );
+    setCharacter(searchResult);
+  }, 500);
 
   useEffect(() => {
     getCharacters();
@@ -60,7 +77,12 @@ const CharactersList = () => {
     </SingleItem>
   ));
 
-  return <List>{charactersRender}</List>;
+  return (
+    <Wrapper>
+      <SearchCharacter searchCharacter={searchCharacter} />
+      <List>{charactersRender}</List>
+    </Wrapper>
+  );
 };
 
 export default CharactersList;
---
+++ src/components/CharactersList/SearchCharacter.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import styled from 'styled-components';
+// import { useRef } from 'react';
+
+const Label = styled.label`
+  color: white;
+  font-size: 22px;
+  text-transform: uppercase;
+  letter-spacing: 5px;
+  display: block;
+  margin: 20px 0;
+`;
+
+const Input = styled.input`
+  padding: 10px 20px;
+  border: none;
+  border-radius: 10px;
+  width: 60%;
+  font-size: 20px;
+  box-shadow: 0 7px 13px 2px #000;
+`;
+
+const SearchCharacter = ({ searchCharacter }) => {
+  const handleSearch = value => {
+    searchCharacter(value);
+  };
+
+  return (
+    <>
+      <Label htmlFor='name'>Wyszukaj postać</Label>
+      <Input id='name' onChange={e => handleSearch(e.target.value)} />
+    </>
+  );
+};
+
+export default SearchCharacter;
